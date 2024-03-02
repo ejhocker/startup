7c668f259d22bb2cc966e71c44e6f8c5f84a598b
ejhocker <emmakinz66@gmail.com>
2024-03-02 00:48:42 -0700

    reversable checks did not work, so back to OG checks

--- mastery.js
+++ mastery.js
@@ -1,7 +1,3 @@
 function changeImage(id) {
-    if (document.getElementById(id).src === "emptyBone.JPG"){
-        document.getElementById(id).src = "yellowBone.JPG";
-    } else {
-        document.getElementById(id).src = "emptyBone.JPG";
-    }
+    document.getElementById(id).src = "yellowBone.JPG";
 }
