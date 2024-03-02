431402cb6aad88fa96f3dfbe9a0d9b47af2bd428
ejhocker <emmakinz66@gmail.com>
2024-03-01 23:46:01 -0700

    fixed player name continuance

--- tricks.js
+++ tricks.js
@@ -2,5 +2,5 @@ function getPlayerName() {
     return localStorage.getItem('userName') ?? 'Unknown User';
 }
 
-const playerName = document.querySelector('.user-name');
+const playerName = document.querySelector('.User');
 playerName.textContent = this.getPlayerName();
