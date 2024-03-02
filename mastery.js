e4c8f0c39196d0849308b8f85ad5cf8717ef7c8c
ejhocker <emmakinz66@gmail.com>
2024-03-02 02:22:04 -0700

    Fixed data continuance of checked boxes

--- mastery.js
+++ mastery.js
@@ -2,7 +2,7 @@ function alreadyLearned() {
     let trickIdList = ["checkSit", "checkStay", "checkLayDown", "checkRollover", "checkShake", "checkSpin", "checkTouch", "checkSpeak", "checkSitPretty"]
     for (var i = 0; i < trickIdList.length; i++){
         let isChecked = localStorage.getItem(trickIdList[i]);
-        if (isChecked === true) {
+        if (isChecked === "true") {
             changeImage(trickIdList[i]);
         }
     }
@@ -12,17 +12,3 @@ function changeImage(id) {
     document.getElementById(id).src = "yellowBone.JPG";
     localStorage.setItem(id, true);
 }
-
-function checkReport() {
-    let trickIdList = ["checkSit", "checkStay", "checkLayDown", "checkRollover", "checkShake", "checkSpin", "checkTouch", "checkSpeak", "checkSitPretty"]
-    let report;
-    for (var i = 0; i < trickIdList.length; i++){
-        if (localStorage.getItem(trickIdList[i] === true)) {
-            report = "You have previously mastered the following tricks: " + trickIdList[i];
-        } else {
-            report = "Master a trick by clicking a bone!";
-        }
-    }
-    document.querySelector("#check-Report").textContent = report;
-}
-checkReport();
