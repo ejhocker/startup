44457e8c417c48fa0168ffa27fe4e3cdff10d57a
ejhocker <emmakinz66@gmail.com>
2024-03-02 01:57:58 -0700

    attempt to make checked tricks permiate

--- mastery.js
+++ mastery.js
@@ -1,3 +1,14 @@
+function alreadyLearned() {
+    let trickIdList = ["checkSit", "checkStay", "checkLayDown", "checkRollover", "checkShake", "checkSpin", "checkTouch", "checkSpeak", "checkSitPretty"]
+    for (var i = 0; i < trickIdList.length; i++){
+        let isChecked = localStorage.getItem(trickIdList[i]);
+        if (isChecked === true) {
+            changeImage(trickIdList[i]);
+        }
+    }
+}
+
 function changeImage(id) {
     document.getElementById(id).src = "yellowBone.JPG";
+    localStorage.setItem(id, true);
 }
