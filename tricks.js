f04d482e464886673c1769fcb6a3cb9b86d50bd2
ejhocker <emmakinz66@gmail.com>
2024-03-02 00:04:52 -0700

    completed the websocket placeholder

--- tricks.js
+++ tricks.js
@@ -4,3 +4,10 @@ function getPlayerName() {
 
 const playerName = document.querySelector('.User');
 playerName.textContent = this.getPlayerName();
+
+setInterval(() => {
+    const notificationAlert = document.querySelector('#notification');
+    notificationAlert.innerHTML =
+      `<li><span class="user-name">Jack</span> learned a new trick!</li>` +
+      notificationAlert.innerHTML;
+  }, 5000);
